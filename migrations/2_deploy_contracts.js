ee8cbadcb8d0ca6b9e4def56e42c66ed34f1bf67
aayushrai <rai.aayush2000@gmail.com>
2021-11-23 20:18:21 +0530

    create Dapp Token and transfer to EthSwap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,16 @@
 const EthSwap = artifacts.require("EthSwap");
+const Token = artifacts.require("Token");
 
-module.exports = function(deployer) {
-  deployer.deploy(EthSwap);
+module.exports = async function(deployer) {
+  // Deploy EthSwap
+  await deployer.deploy(EthSwap);
+  const ethSwap = await EthSwap.deployed();
+
+  // Deploy Token
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+
+  // Transfer all token to ethSwap
+
+  await token.transfer(ethSwap.address, '1000000000000000000000000')
 };
